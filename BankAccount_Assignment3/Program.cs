641b5e5d390204ae7ffa8de3d4a71309025c4eb3
IsaMelendezT <157085293+IsaMelendezT@users.noreply.github.com>
2024-03-27 20:30:09 -0400

    Fields and Properties Definition

--- BankAccount_Assignment3/Program.cs
+++ BankAccount_Assignment3/Program.cs
@@ -10,10 +10,37 @@ namespace BankAccount_Assignment3
     {
         public class BankAccount
         {
+            // Fields Declaration 
+            private int accountNumber;
+            private string type = "Checking";
+            private double balance = 0.0; 
+
+            // Properties
+            public int AccountNumber 
+            { 
+                get { return accountNumber; } 
+                set {  accountNumber = value; } 
+            }
+
+            public double Balance
+            {
+                get { return balance; }
+                set { balance = value; }
+            }
+
+            public string Type 
+            {
+                get { return type; }
+                set { type = value; }
+            } 
+
+
+            
 
         }
         static void Main(string[] args)
         {
+
 
 
         }
